af5028508db804665a9cfdc2b4db2cf11d78e438
rhythmsosad <rhythmsosad@gmail.com>
2015-03-20 18:36:50 +0700

    Add Import Controller and View (not finished)

---
+++ RoomUsageApp/RoomUsageApp/Controllers/ImportController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomUsageApp.Controllers
+{
+    public class ImportController : Controller
+    {
+        // GET: Import
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUploadHandler()
+        {
+            foreach (var fileKey in Request.Files.AllKeys)
+            {
+                var file = Request.Files[fileKey];
+                try
+                {
+                    if (file != null)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/FileDirectory/uploads"), fileName);
+                        file.SaveAs(path);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { Message = "Error in saving file" });
+                }
+            }
+            return Json(new { Message = "File saved" });
+        }
+    }
+}
