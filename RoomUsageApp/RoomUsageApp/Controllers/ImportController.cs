29cf5ef4d7907a25e841f7bc78c07c31feab6156
rhythmsosad <rhythmsosad@gmail.com>
2015-03-21 20:44:48 +0700

    [Import] Read excel stream and display as table

--- RoomUsageApp/RoomUsageApp/Controllers/ImportController.cs
+++ RoomUsageApp/RoomUsageApp/Controllers/ImportController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,9 +28,46 @@ namespace RoomUsageApp.Controllers
                 {
                     if (file != null)
                     {
-                        var fileName = Path.GetFileName(file.FileName);
-                        var path = Path.Combine(Server.MapPath("~/FileDirectory/uploads"), fileName);
-                        file.SaveAs(path);
+                        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(file.InputStream);
+
+                        excelReader.IsFirstRowAsColumnNames = true;
+                        DataSet result = excelReader.AsDataSet();
+
+                        // Format to display row data
+                        StringBuilder sbRowFormat = new StringBuilder();
+                        int columnCount = 0;
+                        string rowFormat = string.Empty;
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("<table class=\"table table-striped table-bordered table-hover dataTables-example\">");
+                        sb.Append("<thead>");
+                        sb.Append("<tr>");
+
+                        sbRowFormat.Append("<tr>");
+                        foreach(DataColumn column in result.Tables[0].Columns)
+                        {
+                            sb.AppendFormat("<th>{0}</th>", column.ColumnName);
+                            sb.Append("");
+
+                            sbRowFormat.AppendFormat("<td>{{{0}}}</td>", columnCount++);
+                        }
+                        sbRowFormat.Append("</tr>");
+                        rowFormat = sbRowFormat.ToString();
+
+                        sb.Append("</tr>");
+                        sb.Append("<tbody>");
+                        foreach(DataRow row in result.Tables[0].Rows)
+                        {
+                            sb.AppendFormat(rowFormat, row.ItemArray);
+                        }
+                        sb.Append("</tbody>");
+                        sb.Append("</thead>");
+                        sb.Append("</table>");
+
+                        excelReader.Close();
+
+                        string html = sb.ToString();
+                        return Json(new { Message = html });
                     }
                 }
                 catch (Exception ex)
@@ -35,7 +75,7 @@ namespace RoomUsageApp.Controllers
                     return Json(new { Message = "Error in saving file" });
                 }
             }
-            return Json(new { Message = "File saved" });
+            return Json(new { Message = "Error in saving file" });
         }
     }
 }
