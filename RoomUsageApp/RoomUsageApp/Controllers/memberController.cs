0def3d564b78c9eeccd7608d98699c2398ae7a5e
kudokung <piyaphanpongpradit@gmail.com>
2015-05-03 15:31:50 +0700

    #ton fix 20150305

---
+++ RoomUsageApp/RoomUsageApp/Controllers/memberController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoomUsageApp.Models;
+
+namespace RoomUsageApp.Controllers
+{
+    public class memberController : Controller
+    {
+        // GET: ton
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult testPage01()
+        {
+            memberClass model = new memberClass();
+          
+            model.name = "TestName";
+            model.surname = "TestSurname";
+
+           // model.testList
+
+            return View(model);
+        }
+    }
+}
