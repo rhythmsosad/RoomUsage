144153ad0da94a0c2272c5b9e2123617277c9469
Sakkachin Wongcharoen <rhythmsosad@gmail.com>
2015-05-03 13:16:30 +0700

    [draft]

---
+++ RoomUsageApp/RoomUsageApp/Controllers/Test2Controller.cs
@@ -0,0 +1,21 @@
+﻿using RoomUsageApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomUsageApp.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        // GET: Test2
+        public ActionResult Index()
+        {
+            Class1 model = new Class1();
+            model.name = "Name";
+            model.surname = "Surname";
+            return View(model);
+        }
+    }
+}
---
+++ RoomUsageApp/RoomUsageApp/Models/Class1.cs
@@ -0,0 +1,24 @@
+﻿using RoomUsageApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomUsageApp.Models
+{
+    public class Class1
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        public void Query() {
+            using (var entity = new DB_CHINEntities())
+            {
+                var obj = from o in entity.Building
+                          select o;
+                obj.ToList();
+            }
+        }
+
+    }
+}
