d93dc9c84a6d20d094084761d979d8afbc53a289
rhythmsosad <rhythmsosad@gmail.com>
2015-05-15 13:14:22 +0700

    Add Lut for Room

---
+++ RoomUsageApp/RoomUsageApp/Controllers/RoomController.cs
@@ -0,0 +1,26 @@
+﻿using RoomUsageApp.Models;
+using RoomUsageApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomUsageApp.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RoomLut(string buildingNo)
+        {
+            RoomModel model = new RoomModel();
+            List<Room> result = model.QueryLutRoom(buildingNo);
+            return Json(result.Select(o => new { RoomId = o.Id, RoomNo = o.RoomNo }));
+        }
+    }
+}
---
+++ RoomUsageApp/RoomUsageApp/Models/RoomModel.cs
@@ -0,0 +1,23 @@
+﻿using RoomUsageApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomUsageApp.Models
+{
+    public class RoomModel
+    {
+        public List<Room> QueryLutRoom(string buildingNo)
+        {
+            using (var entities = new DB_CHINEntities())
+            {
+                var item = from o in entities.Room
+                           where o.BuildingNo == buildingNo
+                           select o;
+
+                return item.ToList();
+            }
+        }
+    }
+}
