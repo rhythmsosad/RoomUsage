b9befff7cf6fd108f821ccba8e003a71a5e27191
rhythmsosad <rhythmsosad@gmail.com>
2015-05-15 13:36:00 +0700

    Add ReportTableModel

---
+++ RoomUsageApp/RoomUsageApp/Models/ReportTableModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomUsageApp.Models
+{
+    public class ReportTableModel
+    {
+        string BuildingNo { get; set; }
+        string RoomNo { get; set; }
+        string[] Days { get; set; }
+        string StartTime { get; set; }
+        string EndTime { get; set; }
+    }
+}
