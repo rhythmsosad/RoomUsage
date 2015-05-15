d77f8139ce74add575aee717899644d199df9f6e
carnat <Artinat_W@yahoo.com>
2015-05-15 11:34:53 +0700

    add check if numberic value on TOTALREG REALREG COURSECOE SECTION FACCODE

--- RoomUsageApp/RoomUsageApp/Classes/teeValidaton.cs
+++ RoomUsageApp/RoomUsageApp/Classes/teeValidaton.cs
@@ -3,24 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data; // require อันนี้เสมอ เพราะจะใช้ .AsEnumurable
+using System.Text.RegularExpressions;
 
 namespace RoomUsageApp.Classes
 {
     public class teeValidation : ValidationBase
     {
-        // สมมติคลาสนี้จะเช็คว่า Course Code ห้ามเกิน 6 หลัก (สมมตินะ)
+        Regex NumberPattern = new Regex("^[0-9]+$");
+        bool Valid;
         public override bool IsValid()
         {
-            int invalidRow = data.AsEnumerable().Where(o => o["COURSECODE"].ToString().Length > 6).Count();
-            if(invalidRow > 0)
+            
+            //Check CourseCode
+            var queryCourseCode = data.AsEnumerable().Where(r => (r["COURSECODE"].ToString() !=""));
+            if (queryCourseCode.Count() > 0)
             {
-                Message = string.Format("มีแถวที่ Course Code เกินกว่า 6 หลักจำนวน {0} แถว กรุณาตรวจสอบและนำเข้าอีกครั้ง", invalidRow);
-                return false;
+                DataTable Validate = queryCourseCode.CopyToDataTable();
+                queryCourseCode = Validate.AsEnumerable().Where(
+                    r => !(NumberPattern.IsMatch(r["COURSECODE"].ToString())));
+               
             }
-            else
+            //Check Realreg
+            var queryRealreg = data.AsEnumerable().Where(r => (r["REALREG"].ToString() != ""));
+            if (queryRealreg.Count() > 0)
+            {
+                DataTable Validate = queryRealreg.CopyToDataTable();
+                queryRealreg = Validate.AsEnumerable().Where(
+                    r => !(NumberPattern.IsMatch(r["REALREG"].ToString())));
+            }
+            //Check TotalReg
+            var queryTotalReg = data.AsEnumerable().Where(r => (r["TOTALREG"].ToString() != ""));
+            if (queryTotalReg.Count() > 0)
+            {
+                DataTable Validate = queryTotalReg.CopyToDataTable();
+                queryTotalReg = Validate.AsEnumerable().Where(
+                    r => !(NumberPattern.IsMatch(r["TOTALREG"].ToString())));
+            }
+            //Check Section
+            var querySection = data.AsEnumerable().Where(r => (r["SECTION"].ToString() != ""));
+            if (querySection.Count() > 0)
+            {
+                DataTable Validate = querySection.CopyToDataTable();
+                querySection = Validate.AsEnumerable().Where(
+                    r => !(NumberPattern.IsMatch(r["SECTION"].ToString())));
+            }
+            //Check Faccode
+            var queryFaccode = data.AsEnumerable().Where(r => (r["FACCODE"].ToString() != ""));
+            if (queryFaccode.Count() > 0)
+            {
+                DataTable Validate = queryFaccode.CopyToDataTable();
+                queryFaccode = Validate.AsEnumerable().Where(
+                    r => !(NumberPattern.IsMatch(r["FACCODE"].ToString())));
+            }
+            if (queryTotalReg.Count() > 0)
+            {
+                Message += string.Format("<br>มีจำนวนผู้ลงทะเบียนได้จำนวน {0} รายการไม่อยู่ในรูปแบบตัวเลข กรุณาตรวจสอบและนำเข้าอีกครั้ง", queryTotalReg.Count());
+                Valid = false;
+            }
+            if (queryRealreg.Count() > 0)
+            {
+                Message += string.Format("<br>มีจำนวนผู้ลงทะเบียนจริงจำนวน {0} รายการไม่อยู่ในรูปแบบตัวเลข กรุณาตรวจสอบและนำเข้าอีกครั้ง", queryRealreg.Count());
+                Valid = false;
+            }
+            if (queryCourseCode.Count() > 0)
+            {
+                Message += string.Format("<br>มีรหัสวิชาจำนวน {0} รายการไม่อยู่ในรูปแบบตัวเลข กรุณาตรวจสอบและนำเข้าอีกครั้ง", queryCourseCode.Count());
+                Valid = false;
+            }
+            if (querySection.Count() > 0)
+            {
+                Message += string.Format("<br>มีตอนเรียนได้จำนวน {0} รายการไม่อยู่ในรูปแบบตัวเลข กรุณาตรวจสอบและนำเข้าอีกครั้ง", querySection.Count());
+                Valid = false;
+            }
+            if (queryFaccode.Count() > 0)
+            {
+                Message += string.Format("<br>มีรหัสคณะได้จำนวน {0} รายการไม่อยู่ในรูปแบบตัวเลข กรุณาตรวจสอบและนำเข้าอีกครั้ง", queryFaccode.Count());
+                Valid = false;
+            }
+            if (Valid)
             {
                 return true;
             }
+                else
+            {
+                return false;
+            }
+            }
         }
     }
-}
